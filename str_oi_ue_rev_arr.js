8a29910d626cddbfc92fd147d3143ca7285f05c1
Marlon Villarama <mvillarama@scalenorthadvisors.com>
2023-03-14 21:02:17 +0800

    20230314 2102

---
+++ str_oi_ue_rev_arr.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright 2023 Stratus-Sphere
+ * All rights reserved
+ * 
+ * Summary:
+ * Data fix script to set Charge Rule stage on Charge records.
+ * 
+ * @NApiVersion         2.1
+ * @NScriptType         MapReduceScript
+ * @NModuleScope        SameAccount
+ * @author              mvillarama
+ * 
+ * Date                 Summary
+ * =====================================================================
+ * 2023/03/13           Initial version
+ * 
+ */
