701ed4f46655f8a921294524b6d3fa13e2e6dc32
Marlon Villarama <marlonsvillarama@gmail.com>
2024-01-31 18:49:28 +1300

    WIF project init

---
+++ src/FileCabinet/SuiteScripts/SS/WIF/src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+    import Card from "./components/shared/Card.svelte";
+    import Dropdown from "./components/shared/Dropdown.svelte";
+
+    let currencies = [
+        { text: 'United States Dollar', value: 'usd' },
+        { text: 'Argentine Peso', value: 'ars' },
+        { text: 'Australian Dollar', value: 'aud' },
+        { text: 'Canada Dollar', value: 'cad' },
+        { text: 'Chilean Peso', value: 'clp' },
+        { text: 'Euro', value: 'eur' },
+        { text: 'British Pound', value: 'gbp' },
+        { text: 'Hong Kong Dollar', value: 'hkd' },
+        { text: 'Mexican Peso', value: 'mxn' },
+        { text: 'New Zealand Dollar', value: 'nzd' },
+        { text: 'Singapore Dollar', value: 'sgd' },
+        { text: 'other', value: 'other' },
+    ];
+</script>
+
+<main>
+    <Card title="Select Currency">
+        <Dropdown id="currency"
+            items={currencies}
+        />
+    </Card>
+</main>
+
+<style>
+</style>
---
+++ src/FileCabinet/SuiteScripts/SS/WIF/src/components/shared/Card.svelte
@@ -0,0 +1,35 @@
+<script>
+    export let title = '[Title]';
+</script>
+
+<div class="card">
+    <div class="title">Select Currency</div>
+    <div class="content">
+        <slot />
+    </div>
+</div>
+
+<style>
+    .card {
+        width: 100%;
+        background-color: white;
+        border-radius: 8px;
+        margin: 0;
+        padding: 0;
+    }
+    .card > * {
+        padding: 12px 18px;
+    }
+    .card > .title {
+        background-color: rgb(135, 91, 5);
+        border-top-left-radius: 8px;
+        border-top-right-radius: 8px;
+        color: white;
+        font-size: 1rem;
+        font-weight: bold;
+    }
+    .card > .content {
+        font-size: 0.875rem;
+        color: #333;
+    }
+</style>
