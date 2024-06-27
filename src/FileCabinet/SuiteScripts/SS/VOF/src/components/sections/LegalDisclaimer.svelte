547f9427efd5b3deacdb3ff8fae636e17954c3c7
Marlon Villarama <marlonsvillarama@gmail.com>
2024-06-27 21:25:23 +1200

    Legal Disclaimer merge

--- src/FileCabinet/SuiteScripts/SS/VOF/src/components/sections/LegalDisclaimer.svelte
+++ src/FileCabinet/SuiteScripts/SS/VOF/src/components/sections/LegalDisclaimer.svelte
@@ -22,32 +22,4 @@
     />
 </Card>
 
-<Card {id} {title}>
-    <Blurb id="blurb_legal_disclaimer">
-        <span class="title">Legal Disclaimer</span>
-        <span class="blurb">I (we) hereby authorize Outside Interactive Inc. or its affiliated entities (THE COMPANY) to initiate entries to my (our) checking/savings accounts at the financial institution listed below (THE FINANCIAL INSTITUTION), and, if necessary, initiate adjustments for any transactions credited/debited in error. This authority will remain in effect until THE COMPANY is notified by me (us) in writing to cancel it in such time as to afford THE COMPANY and THE FINANCIAL INSTITUTION a reasonable opportunity to act on it.</span>
-    </Blurb>
-    
-    <InputCheckbox
-        id="{$formFields.LEGAL_DISCLAIMER}"
-        label="Do you agree?"
-        required={true}
-        bind:checked={$formValues[$formFields.LEGAL_DISCLAIMER]}
-    />
-</Card>
-
-<Card {id} {title}>
-    <Blurb id="blurb_legal_disclaimer">
-        <span class="title">Legal Disclaimer</span>
-        <span class="blurb">I (we) hereby authorize Outside Interactive Inc. or its affiliated entities (THE COMPANY) to initiate entries to my (our) checking/savings accounts at the financial institution listed below (THE FINANCIAL INSTITUTION), and, if necessary, initiate adjustments for any transactions credited/debited in error. This authority will remain in effect until THE COMPANY is notified by me (us) in writing to cancel it in such time as to afford THE COMPANY and THE FINANCIAL INSTITUTION a reasonable opportunity to act on it.</span>
-    </Blurb>
-
-    <InputCheckbox
-        id="{$formFields.LEGAL_DISCLAIMER}"
-        label="Do you agree?"
-        required={true}
-        bind:checked={$formValues[$formFields.LEGAL_DISCLAIMER]}
-    />
-
-</Card>
 <style></style>
